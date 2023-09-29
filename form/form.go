e80eded69014cd06900b7d3be4b41104693b6208
Antonio Pagano <apagano@wawand.co>
2023-09-29 10:21:38 -0700

    task: adding decoder method descriptions

--- form/form.go
+++ form/form.go
@@ -10,13 +10,21 @@ import (
 
 // use a single instance of Decoder, it caches struct info
 var (
+	// Shared decoder instance with default options from
+	// the underlying library.
 	decoder = form.NewDecoder()
 )
 
+// RegisterCustomTypeFunc registers a custom type decoder func for a type.
+// This is useful when you want to use a custom type or a type from an external
+// package like uuid.UUID and want to decode it from a string.
 func RegisterCustomTypeFunc(fn form.DecodeCustomTypeFunc, kind interface{}) {
 	decoder.RegisterCustomTypeFunc(fn, kind)
 }
 
+// Decode decodes the request body into dst, which must be a pointer of a struct.
+// If there is no body or the body is empty, it will take the query string as the
+// body. If the Content-Type is multipart/form-data.
 func Decode(r *http.Request, dst interface{}) error {
 	//MultipartForm
 	if strings.Contains(r.Header.Get("Content-Type"), "multipart/form-data") {
