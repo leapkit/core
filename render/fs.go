77d2b7cda536b12ea029d2236bf1b82519f260c2
Antonio Pagano <apagano@wawand.co>
2024-02-27 10:23:43 -0500

    feature: adding templateFS and removing mdfs

--- render/fs.go
+++ render/fs.go
@@ -1,20 +1,18 @@
-// package mdfs provides a file system that wraps a directory and an embed.FS.
-// it prioritizes the directory FS and falls back to the embedded FS if the file cannot
-// be found on disk. This is useful during development or when deploying with
-// assets not embedded in the binary.
-package mdfs
+package render
 
 import (
 	"io"
 	"io/fs"
 	"os"
+
+	"github.com/leapkit/core/envor"
 )
 
-// FS wraps a directory and an embed FS that are expected to have the same contents.
-// it prioritizes the directory FS and falls back to the embedded FS if the file cannot
+// templatesFS wraps a directory and an embed templatesFS that are expected to have the same contents.
+// it prioritizes the directory templatesFS and falls back to the embedded templatesFS if the file cannot
 // be found on disk. This is useful during development or when deploying with
 // assets not embedded in the binary.
-type FS struct {
+type templatesFS struct {
 	dir string
 
 	embed fs.FS
@@ -25,14 +23,15 @@ type FS struct {
 
 // NewFallbackFS returns a new FS that wraps the given directory and embedded FS.
 // the embed.FS is expected to embed the same files as the directory FS.
-func New(embed fs.FS, dir, env string) FS {
+func TemplateFS(embed fs.FS, dir string) templatesFS {
 	// If the directory is empty, use the current working directory.
 	if dir == "" {
 		pwd, _ := os.Getwd()
 		dir = pwd
 	}
 
-	return FS{
+	env := envor.Get("GO_ENV", "development")
+	return templatesFS{
 		embed: embed,
 		dirFs: os.DirFS(dir),
 		dir:   dir,
@@ -50,7 +49,7 @@ func New(embed fs.FS, dir, env string) FS {
 // Open should reject attempts to open names that do not satisfy
 // ValidPath(name), returning a *PathError with Err set to ErrInvalid or
 // ErrNotExist.
-func (f FS) Open(name string) (file fs.File, err error) {
+func (f templatesFS) Open(name string) (file fs.File, err error) {
 	if f.useLocal {
 		file, err = f.dirFs.Open(name)
 		if err == nil {
@@ -64,7 +63,7 @@ func (f FS) Open(name string) (file fs.File, err error) {
 
 // ReadFile reads the named file from the file system fs and returns its contents.
 // It uses the custom Open method to open the file.
-func (f FS) ReadFile(name string) ([]byte, error) {
+func (f templatesFS) ReadFile(name string) ([]byte, error) {
 	x, err := f.Open(name)
 	if err != nil {
 		return nil, err
